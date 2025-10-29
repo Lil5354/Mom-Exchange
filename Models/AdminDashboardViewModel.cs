49f9d46ec38d6f05812144fc99344f1148832baa
Lil5354 <dttthao.5354@gmail.com>
2025-10-30 00:55:50 +0700

    Fix CS0117 errors: Add missing properties to AdminDashboardViewModel

--- Models/AdminDashboardViewModel.cs
+++ Models/AdminDashboardViewModel.cs
@@ -7,9 +7,21 @@ namespace B_M.Models
         public int TotalUsers { get; set; }
         public int ActiveUsers { get; set; }
         public int AdminUsers { get; set; }
+        public int MomUsers { get; set; }
         public int ClientUsers { get; set; }
         public int NewUsersThisMonth { get; set; }
         public List<User> RecentUsers { get; set; } = new List<User>();
+
+        // Milk Donation Statistics
+        public int TotalMilkPosts { get; set; }
+        public int ActiveMilkPosts { get; set; }
+        public int Tier1Users { get; set; }
+        public int Tier2Users { get; set; }
+        public int PendingMedicalRecords { get; set; }
+
+        // Category Statistics
+        public int TotalCategories { get; set; }
+        public int ActiveCategories { get; set; }
     }
 }
 
